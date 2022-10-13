3d5948b0348cb6b7a84053bc367f2276324a374e
yehyaGewily <103772686+yehyaGewily@users.noreply.github.com>
2022-10-13 04:02:07 +0200

    Delete src directory

--- src/main/java/gwyehya/employeecruddemo/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package gwyehya.employeecruddemo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import gwyehya.employeecruddemo.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
